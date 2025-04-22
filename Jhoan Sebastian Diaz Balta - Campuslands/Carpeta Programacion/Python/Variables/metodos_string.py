1c7f22d4317b569964e0d008ce07cce279c81fb3
JhoanS5 <JhoanS5.ing@gmail.com>
2025-04-22 16:55:31 -0500

    Agregue la carpeta de python para agregar contenido

---
+++ Jhoan Sebastian Diaz Balta - Campuslands/Carpeta Programacion/Python/Variables/metodos_string.py
@@ -0,0 +1,7 @@
+# lower() Convertir a minúsculas
+# upper() Convertir a mayúsculas
+# strip() Borrar espacios iniciales y finales
+# find() Encontrar similitud en cadena de texto
+# join() unir cadenas iterables
+# replace() Reemplaza parte de la cadena
+# split() Dividir cadenas
