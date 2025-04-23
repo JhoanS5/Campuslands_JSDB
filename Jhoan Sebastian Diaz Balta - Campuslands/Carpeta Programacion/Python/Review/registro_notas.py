553898bf00dfdf853d89f05c3cc24a224897f710
JhoanS5 <JhoanS5.ing@gmail.com>
2025-04-23 14:55:51 -0500

    Mejora a ejercicio review

--- Jhoan Sebastian Diaz Balta - Campuslands/Carpeta Programacion/Python/Review/registro_notas.py
+++ Jhoan Sebastian Diaz Balta - Campuslands/Carpeta Programacion/Python/Review/registro_notas.py
@@ -1,15 +1,17 @@
 #Registro de Notas de Estudiantes
 
 for i in range(3):
+    suma = 0
+    promedio = 0
     nombre = input(f"Ingrese el nombre del estudiante #{i+1}: ")
+    
+    for j in range(3):
+        nota = float(input(f"Ingrese la {j+1} nota: "))
+        suma += nota
 
-    nota1 = float(input(f"Ingrese la primer nota de {nombre}: "))
-    nota2 = float(input(f"Ingrese la segunda nota de {nombre}: "))
-    nota3 = float(input(f"Ingrese la tercera nota de {nombre}: "))
-
-    promedio = (nota1 + nota2 + nota3) / 3
+    promedio = suma / 3
     promedio_redondeado = round(promedio, 2)
-
+    
     if promedio >= 60:
         print(f"{nombre} aprobo con un promedio de {promedio_redondeado}")
     else:
