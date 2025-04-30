df42873be5fb9f70b72d20a93e92f0f153939bf8
JhoanS5 <JhoanS5.ing@gmail.com>
2025-04-29 20:26:44 -0500

    Nuevos cambios

--- Jhoan Sebastian Diaz Balta - Campuslands/Carpeta Programacion/Python/Listas/metodos_principales.py
+++ Jhoan Sebastian Diaz Balta - Campuslands/Carpeta Programacion/Python/Listas/metodos_principales.py
@@ -5,7 +5,7 @@ x.count(e) - Conteo de instancias de e en la lista x.
 x.remove(e) - Elimina el primer elemento e de la lista x.
 x.pop(1) - Entrega el elemento en la posicion i en la lista y lo elimina.
 x.index(e) - Entrega la posicion del primer elemento e en x.
-x.sort() - Ordena x en forma creciente.
+x.sort() - Ordena x en forma creciente. para que sea descendete se mete en el argumento x.sort(reverse = True)
 x.reverse() - Invierte la lista x
 x.clear() - Vaciar la lista x
 x.copy() - Entrega una copia por referencia de una lista de un nivel.
